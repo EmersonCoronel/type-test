e6144a95d92a80f0ba66eebf1f974a1dd89269fc
Emerson Coronel <dev@emersoncoronel.com>
2024-10-14 13:53:39 -0500

    trying to get working with next

--- dev/index.tsx
+++ dev/index.tsx
@@ -1,8 +1,9 @@
-export { default as TypeTest } from './TypeTest';
+export { default as TypeTest } from '../src/TypeTest';
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import TypeTest from "./TypeTest";
+import TypeTest from "../src/TypeTest";
+import "./styles.css"
 
 const rootElement = document.getElementById("root");
 
