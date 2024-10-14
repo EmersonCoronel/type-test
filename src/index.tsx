4071ffa57f8f9fcf19f7bbc2466f8e10848aa50b
Emerson Coronel <dev@emersoncoronel.com>
2024-10-14 13:36:40 -0500

    removed bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ export { default as TypeTest } from './TypeTest';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import TypeTest from "./TypeTest";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 const rootElement = document.getElementById("root");
 
